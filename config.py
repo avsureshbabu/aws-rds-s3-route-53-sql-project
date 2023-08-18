2df995a9c3d804384ef89c59832518928ee33cc0
avsureshbabu <139893771+avsureshbabu@users.noreply.github.com>
2023-08-18 08:08:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
+customuser = "sureshit"
+custompass = "suresh123"
+customdb = "employee"
+custombucket = "addemployee"
+customregion = "us-east-2"
